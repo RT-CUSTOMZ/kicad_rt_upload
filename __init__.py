a102200d260ff4e88c516e62038220abe9c8f60a
Marvin Gudel <github.marvin@gudel.org>
2024-03-07 18:59:11 +0100

    Improve error handling (#1)
    * Display nice errors on failure and a status page upon success.
    
    * Change default exposure, change file extension to TOP, BOT

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
+from urllib.error import HTTPError
 import wx
 import wx.aui
+import wx.adv
 import pcbnew
 import os
 from urllib.request import *
@@ -8,7 +10,8 @@ from pathlib import Path
 
 majorVersion = int(pcbnew.Version().split(".")[0]) 
 FILEPATH = 'uv_export/'
-BASE_URL = 'http://10.10.13.190:8000/upload?'
+BASE_URL = 'http://10.10.13.190:8000/'
+DEFAULT_EXPOSURE = 60
 
 
 def getProjectBasePath():
@@ -16,9 +19,28 @@ def getProjectBasePath():
     parts = board_path.rpartition('/')
     return str(parts[0]+parts[1])
 
+class UploadFinishedDialog(wx.Dialog):
+    def __init__(self, parent, id1, id2):
+        super(UploadFinishedDialog, self).__init__(parent, title = "Upload finished!")
+        panel = wx.Panel(self)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        text = wx.StaticText(panel, -1, "Upload successful, view the status by following the links:")
+        vbox.Add(text)
+        id1_url = wx.adv.HyperlinkCtrl(panel, url = BASE_URL + 'status?upload_id=' + id1.decode())
+        vbox.Add(id1_url)
+        id2_url = wx.adv.HyperlinkCtrl(panel, url = BASE_URL + 'status?upload_id=' + id2.decode())
+        vbox.Add(id2_url)
+        panel.SetSizer(vbox)
+        vbox.SetSizeHints(self)
+
+        
 
 class UploadDialog(wx.Dialog): 
-   def __init__(self, parent, title): 
+    def __init__(self, parent, title, pctl, popt, board): 
+        self.pctl = pctl
+        self.popt = popt
+        self.board = board
         super(UploadDialog, self).__init__(parent, title = title) 
         panel = wx.Panel(self) 
 
@@ -27,7 +49,7 @@ class UploadDialog(wx.Dialog):
         hbox2 = wx.BoxSizer(wx.HORIZONTAL)
 
         username_box = wx.BoxSizer(wx.VERTICAL)
-        username_box.Add(wx.StaticText(panel, -1, "User Name"), flag = wx.ALIGN_LEFT)
+        username_box.Add(wx.StaticText(panel, -1, "User/Project Name"), flag = wx.ALIGN_LEFT)
         self.username = wx.TextCtrl(panel, size = (180, 30))
         username_box.Add(self.username, proportion = 0)
         hbox1.Add(username_box, border = 5, flag = wx.ALL)
@@ -35,7 +57,7 @@ class UploadDialog(wx.Dialog):
 
         exposure_box = wx.BoxSizer(wx.VERTICAL)
         exposure_box.Add(wx.StaticText(panel, -1, "Exposure (seconds)"), flag = wx.ALIGN_LEFT)
-        self.exposure = wx.SpinCtrl(panel, size = (120, 30), min = 1, max = 1200, initial = 15)
+        self.exposure = wx.SpinCtrl(panel, size = (120, 30), min = 1, max = 1200, initial = DEFAULT_EXPOSURE)
         exposure_box.Add(self.exposure, flag = wx.ALIGN_LEFT)
         hbox1.Add(exposure_box, border = 5, flag = wx.ALL)
 
@@ -58,16 +80,26 @@ class UploadDialog(wx.Dialog):
         vbox.Add(hbox2, flag = wx.ALIGN_CENTER)
         vbox.AddSpacer(20)
 
-        self.ok = wx.Button(panel, wx.ID_OK, label = "Upload", size = (70,30))
+        self.upload = wx.Button(panel, label = "Upload", size = (70,30))
         self.cancel = wx.Button(panel, wx.ID_CANCEL, label = "Cancel", size = (70,30))
         confirmation_box = wx.BoxSizer(wx.HORIZONTAL)
         confirmation_box.Add(self.cancel, flag = wx.ALIGN_RIGHT|wx.ALL, border = 5)
-        confirmation_box.Add(self.ok, flag = wx.ALIGN_RIGHT|wx.ALL, border = 5)
+        confirmation_box.Add(self.upload, flag = wx.ALIGN_RIGHT|wx.ALL, border = 5)
 
         vbox.Add(confirmation_box, flag = wx.ALIGN_BOTTOM|wx.ALIGN_RIGHT)
         vbox.SetMinSize((200, 0))
         panel.SetSizer(vbox)
         vbox.SetSizeHints(self)
+
+        self.Bind(wx.EVT_BUTTON, self.onUploadClick, self.upload)
+    def onUploadClick(self, event):
+        generateGerberForUV(self.pctl, self.popt, self.board)
+        # And now upload, display errors in message box if any
+        ids = uploadGerbers(self.username.GetLineText(0), self.exposure.GetValue(), self.x_offset.GetValue(), self.y_offset.GetValue())
+        if len(ids) == 2:
+            UploadFinishedDialog(self, ids[0], ids[1]).ShowModal()
+        self.EndModal(wx.ID_OK)
+
 
         
 
@@ -101,19 +133,20 @@ class RtUvUploadPlugin(pcbnew.ActionPlugin):
 
         popt.SetOutputDirectory(FILEPATH)
 
-        # wx.MessageBox(getProjectBasePath())
-        dlg = UploadDialog(None, "RT UV Upload")
+        if len(getProjectBasePath()) == 0:
+            wx.MessageBox("Failed to evaluate Project Base Path! Please restart KiCad!")
+        dlg = UploadDialog(None, "RT UV Upload", pctl, popt, board)
         if dlg.ShowModal() == wx.ID_OK:
-            generateGerberForUV(pctl, popt, board)
-            # And now upload, display errors in message box if any
-            uploadGerbers(dlg.username.GetLineText(0), dlg.exposure.GetValue(), dlg.x_offset.GetValue(), dlg.y_offset.GetValue())
-                
+            # TODO
+            return
 
 
 plugin = RtUvUploadPlugin()
 plugin.register() 
 
 def uploadGerbers(user, exposure, offset_x, offset_y):
+    ids = []
+
     params = {}
     if user != "":
         params['user'] = user
@@ -123,28 +156,40 @@ def uploadGerbers(user, exposure, offset_x, offset_y):
 
     headers = {'Content-Type': 'text/plain'}
     gbrs = getFilePaths()
+    if len(gbrs) == 0:
+        wx.MessageBox("Can't find Gerber files for upload")
     for f in gbrs:
         with open(file=f, mode='r') as data:
-            if f.name.endswith('F_Cu_UV.gbr'):
+            if f.name.endswith('TOP.gbr'):
                 params['layer'] = 'Top'
-            if f.name.endswith('B_Cu_UV.gbr'):
+            if f.name.endswith('BOT.gbr'):
                 params['layer'] = 'Bottom'
             params['filename'] = f.name
             if not data.readable():
                 wx.MessageBox("Error reading from "+f.name)
-            url = BASE_URL + urllib.parse.urlencode(params)
+            url = BASE_URL + 'upload?' + urllib.parse.urlencode(params)
             req = Request(url, data=data, headers=headers, method="POST")
-            urlopen(req)
+            try:
+                resp = urlopen(req)
+                if resp.status == 202:
+                    ids.append(resp.read())
+            except HTTPError as err:
+                wx.MessageBox("Upload error, code "+str(err.code))
+                return
+            except:
+                wx.MessageBox("Error uploading "+f.name+"\nServer not reachable?")
+                return
             data.close()
+    return ids
 
 def getFilePaths():
     p = Path(getProjectBasePath()+FILEPATH)
     files = list(p.glob('**/*.gbr'))
     gerbers = []
     for f in files:
-        if f.name.endswith('F_Cu_UV.gbr'):
+        if f.name.endswith('TOP.gbr'):
             gerbers.append(f)
-        if f.name.endswith('B_Cu_UV.gbr'):
+        if f.name.endswith('BOT.gbr'):
             gerbers.append(f)
     return gerbers
 
@@ -170,12 +215,12 @@ def generateGerberForUV(plotControl, plotOptions, board):
         plotOptions.SetExcludeEdgeLayer(False)
         plotControl.SetLayer(pcbnew.F_Cu)
         plotControl.OpenPlotfile(
-            "F.Cu.UV", pcbnew.PLOT_FORMAT_GERBER, "Top Copper UV")
+            "TOP", pcbnew.PLOT_FORMAT_GERBER, "Top Copper UV")
         plotControl.PlotLayer()
 
         plotControl.SetLayer(pcbnew.B_Cu)
         plotControl.OpenPlotfile(
-            "B.Cu.UV", pcbnew.PLOT_FORMAT_GERBER, "Bottom Copper UV")
+            "BOT", pcbnew.PLOT_FORMAT_GERBER, "Bottom Copper UV")
         plotControl.PlotLayer()
 
     else:
@@ -184,7 +229,7 @@ def generateGerberForUV(plotControl, plotOptions, board):
         seq.push_back(pcbnew.F_Cu)
         plotControl.SetLayer(pcbnew.F_Cu)
         plotControl.OpenPlotfile(
-            "F.Cu.UV", pcbnew.PLOT_FORMAT_GERBER, "Top Copper UV")
+            "TOP", pcbnew.PLOT_FORMAT_GERBER, "Top Copper UV")
         plotControl.PlotLayers(seq)
 
         seq = pcbnew.LSEQ()
@@ -192,6 +237,6 @@ def generateGerberForUV(plotControl, plotOptions, board):
         seq.push_back(pcbnew.B_Cu)
         plotControl.SetLayer(pcbnew.B_Cu)
         plotControl.OpenPlotfile(
-            "B.Cu.UV", pcbnew.PLOT_FORMAT_GERBER, "Bottom Copper UV")
+            "BOT", pcbnew.PLOT_FORMAT_GERBER, "Bottom Copper UV")
         plotControl.PlotLayers(seq)
     plotControl.ClosePlot()
